70eaab80afeed572c5838a5009f8339caf00038f
Leo Dion <leogdion@brightdigit.com>
2022-12-18 20:22:04 -0500

    Initial Commit

---
+++ WatchMe Watch App/WatchMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchMeApp.swift
+//  WatchMe Watch App
+//
+//  Created by Leo Dion on 12/18/22.
+//
+
+import SwiftUI
+
+@main
+struct WatchMe_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
